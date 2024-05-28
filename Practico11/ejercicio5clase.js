0237f11e166ad8eb6de94469ddb7ed6775eb59d2
Arnould555 <melanyarnould@gmail.com>
2024-05-28 15:05:31 -0300

    ejercicio 5 clase

---
+++ Practico11/ejercicio5clase.js
@@ -0,0 +1,23 @@
+//Crear un objeto que represente los datos de un participante de un juego. Definir las propiedades: nombre, puntos y las funciones: imprimir (mostrar el nombre de jugador y los puntos actuales), aumentarPuntos (permite incrementar la cantidad de puntos actuales del jugador) verificarSiGano (mostrar un alert si los puntos superan a 1000 o no)
+let participante = {
+    nombre: "melany",
+    puntos: 0,
+    imprimir: function() {
+         return "Nombre de jugador: " + this.nombre + "\n Puntos actuales: "+ this.puntos
+    },
+    aumentarPuntos: function() {
+        this.puntos++;
+        return this.puntos;
+    },
+    verificarSiGano: function() {
+        let supera = "El jugador tiene menos de 1000 puntos";
+        if (this.puntos > 1000) {
+            supera = "El jugador tiene mas de 1000 puntos";
+        }
+        console.log(supera);
+    }
+}
+console.log(participante.imprimir())
+console.log(participante.aumentarPuntos())
+console.log(participante.aumentarPuntos())
+console.log(participante.verificarSiGano())
