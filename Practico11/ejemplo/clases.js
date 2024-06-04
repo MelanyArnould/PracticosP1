5e8cc340d2d71b6a20493d38070037f5c20e7016
Arnould555 <melanyarnould@gmail.com>
2024-06-04 11:49:19 -0300

    ejemplo sort

--- Practico11/ejemplo/clases.js
+++ Practico11/ejemplo/clases.js
@@ -6,10 +6,37 @@ class Contacto {
         this.telefono = telefono;
     }
     toString() {
-        this.nombre+ " " + this.apellido+"("+this.edad + ") Tel:" + this.telefono;
+        return this.nombre+ " " + this.apellido+"("+this.edad + ") Tel:" + this.telefono;
     }
     esJoven() {
         return this.edad<25;
+    }
+    darNombreCompleto() {
+        return this.nombre + " " + this.apellido
+    }
+    compararCon(otroContacto) {
+        let resultado
+        if (this.edad < otroContacto.edad) {
+            resultado = -1;
+        } else if (this.edad > otroContacto.edad) {
+            resultado = 1;
+        } else {
+            resultado = 0;
+        }
+        return resultado;
+        //return this.edad-otroContacto.edad;
+    }
+    compararConPorEdadYApellido(otroContacto) {
+        let resultado = this.edad - otroContacto.edad;
+        if (resultado == 0){
+            if(this.apellido < otroContacto.apellido) {
+                resultado = -1;
+            } else if (this.apellido > otroContacto.apellido) {
+                resultado = 1;
+            } else {
+                resultado = 0;
+            }
+        }
     }
 }
 class Agenda {
@@ -18,6 +45,19 @@ class Agenda {
     }
     agregarContacto(unContacto){
         this.listaContactos.push(unContacto);
+        this.listaContactos.sort(function(a,b){return a.compararCon(b);});
+    }
+    darTodos(){
+        return this.listaContactos;
+    }
+    darJovenes() {
+        let resultado = [];
+        for (let unContacto of this.listaContactos) {
+            if (unContacto.esJoven()) {
+                resultado.push(unContacto);
+            }
+        }
+        return resultado;
     }
     
 }
--- Practico11/ejemplo/code.js
+++ Practico11/ejemplo/code.js
@@ -2,8 +2,16 @@ window.addEventListener("load", inicio)
 
 function inicio() {
     document.getElementById("btnAgregar").addEventListener("click", agregar)
+    document.getElementById("idBotonJovenes").addEventListener("click", consultarJovenes)
+    precargarDatos();
 }
 let miAgenda = new Agenda()
+
+function precargarDatos() {
+    miAgenda.agregarContacto(new Contacto("ana", "rodrigurez", 30, "091222333"));
+    miAgenda.agregarContacto(new Contacto("juan", "rodrigurez", 15, "091222777"));
+    actualizar();
+}
 
 function agregar() {
     let unNombre = document.getElementById("nombre").value;
@@ -14,4 +22,39 @@ function agregar() {
     let unContacto = new Contacto(unNombre, unApellido, unaEdad, unTelefono)
     miAgenda.agregarContacto(unContacto)
     console.log(miAgenda)
+    actualizar();
+    document.getElementById("formAgregarContacto").reset();
+}
+function consultarJovenes() {
+    cargarTabla();
+}
+function cargarTabla() {
+    let tablaHtml = document.getElementById("idTablaJovenes");
+    tablaHtml.innerHTML = "";
+    let datos = miAgenda.darJovenes();
+    if (datos.length == 0) {
+        tablaHtml.innerHTML = "No hay ningun contacto joven";
+    } else {
+        for (let elemento of datos) {
+            let fila = tablaHtml.insertRow();
+            let celda = fila.insertCell();
+            celda.innerHTML = elemento.darNombreCompleto();
+            let celda2 = fila.insertCell();
+            celda2.innerHTML = elemento.edad;
+        }
+    }
+}
+function actualizar() {
+    cargarLista();
+}
+function cargarLista() {
+    let listaHtml = document.getElementById("idLista");
+    listaHtml.innerHTML = "";
+    let datos = miAgenda.darTodos();
+    for (let elemento of datos) {
+        let nodoLi = document.createElement("li");
+        let nodoTexto = document.createTextNode(elemento);
+        nodoLi.appendChild(nodoTexto);
+        listaHtml.appendChild(nodoLi);
+    }
 }
